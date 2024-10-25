38e73d3096b6d262c8fbd9acad974c08032dfceb
Christopher Duenas <chrisduenasonline@gmail.com>
2024-10-25 19:09:00 -0400

    created a top level component called Game to lift state up in order to display history of moves

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import {useState} from 'react';
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares,setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    // setSquares(nextSquares);
+    // setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -50,9 +47,11 @@ export default function Board() {
 }
 
 function Square({value, onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>
-          {value}
-         </button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function calculateWinner(squares) {
@@ -75,4 +74,26 @@ function calculateWinner(squares) {
 
   }
   return null;
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board"> 
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{/* To Do*/}</ol>
+      </div>
+    </div>
+  );
 }
